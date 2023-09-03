87f7317a687c978d483c1c645783753548191b33
gansiorag <breslau1962@yandex.ru>
2023-09-03 21:24:55 +0300

    add ch

--- lesson_4.py
+++ lesson_4.py
@@ -1,55 +1,40 @@
-import socket
-import selectors
+from time import time
 
-selector = selectors.DefaultSelector()
-
-def server():
-    HOST = ('localhost', 4875)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(HOST)
-    server_socket.listen()
-    
-    selector.register(fileobj = server_socket, events = selectors.EVENT_READ, data = accept_connection)
-
-
-
-def accept_connection(server_socket):
-    client_socket, addr = server_socket.accept()
-    print('Connection from -> ', addr)
-    selector.register(fileobj = client_socket, events = selectors.EVENT_READ, data = send_message)
-
+def gen1(s):
+    for i in s:
+        yield i
         
-def send_message(client_socket):
-        request = client_socket.recv(4096)
         
-        if request:
-            print(request)
-            response = 'Hello user!!!\n'.encode()
-            client_socket.send(response)
-        else:
-            selectors.unregister(client_socket)
-            client_socket.close()
-    
-def event_loop():
+def gen_filename():
     while True:
-        events = selector.select() # {key, events
-        for key, _ in events:
-            callback = key.data
-            callback(key.fileobj)
+        pattern = 'file_{}.jpeg'
+        t = int(time()*1000)
+        yield pattern.format(str(t))
+        
+        print('Info after yield')
+        
+def gen2(n):
+    for i in range(n):
+        yield i
+        
+        
+g1 = gen1('sasha')
+g2 = gen2(4)
+
+
+tasks = [g1,g2]
+
+while tasks:
+    task = tasks.pop(0)
     
-    
-                
-"""
-We are bloked answer one client 
-
-creater client - new terminal - nc localhost 4875
-
-if we have several clients, server not answer two client while
-first client have connection
-
-"""
-
-if __name__ == '__main__':
-    server()
-    event_loop()
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)              
+    except StopIteration:
+        pass
+          
+        
+        
+        
+        
